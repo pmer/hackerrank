dca39e7ae1e7f9e2ec8da10828a3681f0692b024
Paul Merrill <pdm@pdm.me>
2016-10-02 00:46:59 -0700

    Solve hello-world-n-times in Racket 2

--- functional-programming/introduction/hello-world-n-times/solution.rkt
+++ functional-programming/introduction/hello-world-n-times/solution.rkt
@@ -1,8 +1,14 @@
 #lang racket
-(displayln
-  (string-join
-    (map (λ (_) "Hello World")
-         (stream->list
-           (in-range (string->number
-                       (read-line)))))
-    "\n"))
+(for ([_ (in-range
+           (string->number
+             (read-line)))])
+     (displayln "Hello World"))
+
+;; Alternative solution
+;(displayln
+;  (string-join
+;    (map (λ (_) "Hello World")
+;         (stream->list
+;           (in-range (string->number
+;                       (read-line)))))
+;    "\n"))
